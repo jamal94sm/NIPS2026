6084a5cc25a8cc24b670d31ed2b8727986c2ee83
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2026-02-19 15:49:00 +0800

    Update NIPS_2.py

--- NIPS_2.py
+++ NIPS_2.py
@@ -18,9 +18,9 @@ from pytorch_metric_learning import losses
 batch_size = 32   
 margin = 0.3
 scale = 16
-lr = 1e-3
+lr = 0.5e-3
 weight_decay = 1e-4
-epochs = 50
+epochs = 100
 lamb = 0.2       # Weight for Contrastive Loss
 
 # Choose domains by NAME
