13c566d7c4c642af49772ec66848e7c05a27176f
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2026-03-31 20:27:15 +0800

    Update ffard.py

--- ROI_Extraction/ffard.py
+++ ROI_Extraction/ffard.py
@@ -28,12 +28,12 @@ Output
 # ─────────────────────────────────────────────────────────────────
 #  CONFIG  ← edit these before running
 # ─────────────────────────────────────────────────────────────────
-MPD_RAW_DIR = "/path/to/MPD_RAW"
-CHSST_CKPT  = "/path/to/EP7-iou0.951562-pacc0.977916.pth"
-LANET_CKPT  = "/path/to/LANet_v1.pkl"
+MPD_RAW_DIR = "/home/pai-ng/Jamal/MPDv2"
+CHSST_CKPT  = "EP7-iou0.951562-pacc0.977916.pth"
+LANET_CKPT  = "LANet_v1.pkl"
 SEG_TMP_DIR = "/tmp/MPD_segmented"        # intermediate; can be /tmp
-ROI_OUT_DIR = "/path/to/ROIs/MPD"         # ← main output
-VIS_OUT_DIR = "/path/to/ROIs/MPD_vis"     # circle debug visualisations
+ROI_OUT_DIR = "/home/pai-ng/Jamal/MPDv2_ROI_FFARD"         # ← main output
+VIS_OUT_DIR = "/MPD_vis/"     # circle debug visualisations
 SAVE_VIS    = False                        # set True to write debug images
 DEVICE      = "cuda"                       # "cuda" or "cpu"
 # ─────────────────────────────────────────────────────────────────
