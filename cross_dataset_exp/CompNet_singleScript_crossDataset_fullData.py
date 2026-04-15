779a4144eacecd69034f6db8ed58f80ac230139d
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2026-04-15 13:11:04 +0800

    Update CompNet_singleScript_crossDataset_fullData.py

--- cross_dataset_exp/CompNet_singleScript_crossDataset_fullData.py
+++ cross_dataset_exp/CompNet_singleScript_crossDataset_fullData.py
@@ -103,7 +103,7 @@ ALLOWED_SPECTRA = {"green", "ir", "yellow", "pink", "white"}
 N_HIGH      = 150   # number of IDs in the high-sample group
 N_LOW       = 40    # number of IDs in the low-sample group
 TARGET_HIGH_CASIA = 30
-TARGET_HIGH_MPD   = 33
+TARGET_HIGH_MPD   = 34
 TARGET_LOW        = 15
 
 
