4d48a5ac7a856b83d691c8e099c93ff5f1ae33da
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2026-07-25 16:48:24 +0800

    Update co3net_crossdomain_openset.py

--- cross_domain_open_set/co3net_crossdomain_openset.py
+++ cross_domain_open_set/co3net_crossdomain_openset.py
@@ -55,7 +55,7 @@ CONFIG = {
 
     # Training
     "batch_size"           : 256,
-    "num_epochs"           : 200,
+    "num_epochs"           : 100,
     "lr"                   : 0.001,
     "lr_step"              : 30,
     "lr_gamma"             : 0.6,
